d408fa7add4fb08b410a3ae5465d0f06d6809951
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-02-25 09:07:59 +0530

    Buy and sell stock with cooldown

--- Dynamic Programming/Dp_BuySellStockWithCooldown.java
+++ Dynamic Programming/Dp_BuySellStockWithCooldown.java
@@ -22,4 +22,86 @@ public class Dp_BuySellStockWithCooldown {
 
     return ossp;
   }
+
+  public long maximumProfit(long prices[], int N) {
+
+    // return rec(0, 1, N, prices)
+
+    // long dp[][] = new long[N + 1][2];
+    // for(int i = 0; i <= N; i++){
+    // Arrays.fill(dp[i], -1);
+    // }
+
+    // return mem(0, 1, N, prices, dp);
+
+    // return tab(prices, N);
+
+    return so(prices, N);
+  }
+
+  public long rec(int i, int buy, int n, long prices[]) {
+
+    if (i >= n)
+      return 0;
+
+    if (buy == 1) {
+      return Math.max(-prices[i] + rec(i + 1, 0, n, prices), 0 + rec(i + 1, 1, n, prices));
+    }
+
+    return Math.max(prices[i] + rec(i + 2, 1, n, prices), 0 + rec(i + 1, 0, n, prices));
+  }
+
+  public long mem(int i, int buy, int n, long prices[], long dp[][]) {
+
+    if (i >= n)
+      return 0;
+
+    if (dp[i][buy] != -1)
+      return dp[i][buy];
+
+    if (buy == 1) {
+      return dp[i][buy] = Math.max(-prices[i] + mem(i + 1, 0, n, prices, dp), 0 + mem(i + 1, 1, n, prices, dp));
+    }
+
+    return dp[i][buy] = Math.max(prices[i] + mem(i + 2, 1, n, prices, dp), 0 + mem(i + 1, 0, n, prices, dp));
+
+  }
+
+  public long tab(long prices[], int n) {
+
+    long dp[][] = new long[n + 2][2];
+
+    for (int i = n - 1; i >= 0; i--) {
+      for (int buy = 0; buy <= 1; buy++) {
+
+        if (buy == 1) {
+          dp[i][buy] = Math.max(-prices[i] + dp[i + 1][0], 0 + dp[i + 1][1]);
+        } else {
+          dp[i][buy] = Math.max(prices[i] + dp[i + 2][1], 0 + dp[i + 1][0]);
+        }
+
+      }
+    }
+
+    return dp[0][1];
+  }
+
+  public long so(long prices[], int n) {
+
+    long ahead2[] = new long[2];
+    long ahead1[] = new long[2];
+
+    for (int i = n - 1; i >= 0; i--) {
+      long curr[] = new long[2];
+
+      curr[1] = Math.max(-prices[i] + ahead1[0], 0 + ahead1[1]);
+
+      curr[0] = Math.max(prices[i] + ahead2[1], 0 + ahead1[0]);
+
+      ahead2 = ahead1;
+      ahead1 = curr;
+    }
+
+    return ahead1[1];
+  }
 }
