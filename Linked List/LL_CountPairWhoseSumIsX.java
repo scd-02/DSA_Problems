1e2f4284a3e31fe8b37021da33a16635496265d5
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-03-17 08:38:34 +0530

    count pairs whose sum is equal to x

---
+++ Linked List/LL_CountPairWhoseSumIsX.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LL_CountPairWhoseSumIsX {
+  class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+      data = key;
+      next = null;
+    }
+  }
+
+  public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
+      int x) {
+
+    HashSet<Integer> hs = new HashSet<>();
+
+    int count = 0;
+
+    Iterator<Integer> itr1 = head1.iterator();
+    while (itr1.hasNext()) {
+      hs.add(itr1.next());
+
+    }
+
+    Iterator<Integer> itr2 = head2.iterator();
+    while (itr2.hasNext()) {
+      if (hs.contains(x - itr2.next())) {
+        count++;
+      }
+
+    }
+
+    return count;
+  }
+}
