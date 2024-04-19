7b2f3e1088258d558dba87423f7552640731ce3e
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-04-19 12:11:58 +0530

    top view

---
+++ Tree/Tree_TopView.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class Tree_TopView {
+  static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+      this.data = data;
+      left = null;
+      right = null;
+    }
+  }
+
+  static ArrayList<Integer> topView(Node root) {
+    Map<Integer, Integer> hm = new TreeMap<>();
+    Queue<Pair> q = new LinkedList<>();
+
+    q.add(new Pair(root, 0));
+    hm.put(0, root.data);
+
+    while (!q.isEmpty()) {
+      Pair p = q.poll();
+      Node u = p.node;
+      int line = p.line;
+
+      if (!hm.containsKey(line)) {
+        hm.put(line, u.data);
+      }
+
+      if (u.left != null) {
+        q.add(new Pair(u.left, line - 1));
+      }
+
+      if (u.right != null) {
+        q.add(new Pair(u.right, line + 1));
+      }
+
+    }
+
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    for (int val : hm.values()) {
+
+      ans.add(val);
+    }
+
+    return ans;
+
+  }
+
+  static class Pair {
+    Node node;
+    int line;
+
+    Pair(Node node, int line) {
+      this.node = node;
+      this.line = line;
+    }
+  }
+}
