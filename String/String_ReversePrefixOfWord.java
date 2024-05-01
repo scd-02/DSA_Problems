df30a66b55e3a49b0ed74ccec244a78222cba1d8
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-05-01 18:04:46 +0530

    reverse prefix of word

---
+++ String/String_ReversePrefixOfWord.java
@@ -0,0 +1,12 @@
+public class String_ReversePrefixOfWord {
+  public String reversePrefix(String word, char ch) {
+
+    int firstOccurence = word.indexOf(ch);
+    if (firstOccurence == -1) {
+      return word;
+    }
+
+    StringBuilder prefix = new StringBuilder(word.substring(0, firstOccurence + 1));
+    return prefix.reverse().toString() + word.substring(firstOccurence + 1);
+  }
+}
