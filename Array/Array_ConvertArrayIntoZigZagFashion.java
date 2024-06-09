6c8d1b5a35dd06af1278e0d3a049a96371a44ce9
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-06-09 12:12:33 +0530

    convert array into zigzag fashion

---
+++ Array/Array_ConvertArrayIntoZigZagFashion.java
@@ -0,0 +1,19 @@
+public class Array_ConvertArrayIntoZigZagFashion {
+  public static void zigZag(int n, int[] arr) {
+    
+    for (int i = 1; i < n; i++) {
+      if (i % 2 != 0 && arr[i] < arr[i - 1]) {
+        swap(arr, i, i - 1);
+      } else if (i % 2 == 0 && arr[i] > arr[i - 1]) {
+        swap(arr, i, i - 1);
+      }
+    }
+
+  }
+
+  static void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
