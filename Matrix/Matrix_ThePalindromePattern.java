993e67a4393e3a85f31477735bb95c4d8c8aa949
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-06-28 07:04:52 +0530

    the palindrome pattern

---
+++ Matrix/Matrix_ThePalindromePattern.java
@@ -0,0 +1,50 @@
+public class Matrix_ThePalindromePattern {
+  public String pattern(int[][] arr) {
+
+    // for all rows
+    for (int i = 0; i < arr.length; i++) {
+      if (isRowPalindrome(arr[i])) {
+        return i + " " + "R";
+      }
+    }
+
+    // for all cols
+    for (int j = 0; j < arr[0].length; j++) {
+      if (isColPalindrome(arr, j)) {
+        return j + " " + "C";
+      }
+    }
+
+    return "-1";
+  }
+
+  boolean isRowPalindrome(int row[]) {
+
+    int left = 0, right = row.length - 1;
+
+    while (left <= right) {
+      if (row[left] != row[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+
+  boolean isColPalindrome(int arr[][], int j) {
+
+    int left = 0, right = arr.length - 1;
+
+    while (left <= right) {
+      if (arr[left][j] != arr[right][j]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+}
