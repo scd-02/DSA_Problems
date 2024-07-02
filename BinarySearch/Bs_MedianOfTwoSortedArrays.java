e4f8165761a427246c32c2209f995db388448201
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-07-02 22:06:29 +0530

    median of two sorted arrays

---
+++ BinarySearch/Bs_MedianOfTwoSortedArrays.java
@@ -0,0 +1,57 @@
+public class Bs_MedianOfTwoSortedArrays {
+  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+    int n1 = nums1.length;
+    int n2 = nums2.length;
+
+    if (n1 > n2)
+      return findMedianSortedArrays(nums2, nums1);
+
+    if (n1 == 0 && n2 == 0)
+      return 0;
+
+    if (n1 == 0) {
+      if (n2 % 2 == 0) {
+        return (double) (nums2[n2 / 2] + nums2[(n2 - 1) / 2]) / 2.0;
+      } else {
+        return nums2[n2 / 2];
+      }
+    }
+
+    if (n2 == 0) {
+      if (n1 % 2 == 0) {
+        return (double) (nums1[n1 / 2] + nums1[(n1 - 1) / 2]) / 2.0;
+      } else {
+        return nums1[n1 / 2];
+      }
+    }
+
+    int left = (n1 + n2 + 1) / 2;
+
+    int low = 0, high = n1;
+
+    while (low <= high) {
+
+      int mid1 = (low + high) / 2;
+      int mid2 = left - mid1;
+
+      int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+      int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+      int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+      int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+      if (l1 <= r2 && l2 <= r1) {
+        if ((n1 + n2) % 2 == 1)
+          return Math.max(l1, l2);
+        else
+          return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+      } else if (l1 > r2)
+        high = mid1 - 1;
+      else
+        low = mid1 + 1;
+
+    }
+
+    return 0.0;
+  }
+}
