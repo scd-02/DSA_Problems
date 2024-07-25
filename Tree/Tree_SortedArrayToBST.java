f074f27f2ba64feaa84395afc92d406c0f7c2aa8
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-07-25 06:33:08 +0530

    array to bst

--- Tree/Tree_SortedArrayToBST.java
+++ Tree/Tree_SortedArrayToBST.java
@@ -5,34 +5,68 @@ public class Tree_SortedArrayToBST {
    * o/p: 4,2,1,3,6,5,7
    */
 
-  int k = 0;
-  int ans[];
+  class Solution1 {
+    int k = 0;
+    int ans[];
 
-  public int[] sortedArrayToBST(int[] nums) {
+    public int[] sortedArrayToBST(int[] nums) {
 
-    int size = nums.length;
-    int i;
-    for (i = 0; i < size; i++) {
-      if (Math.abs((size - 1 - i) - i) == 1 || Math.abs((size - 1 - i) - i) == 0) {
-        break;
+      int size = nums.length;
+      int i;
+      for (i = 0; i < size; i++) {
+        if (Math.abs((size - 1 - i) - i) == 1 || Math.abs((size - 1 - i) - i) == 0) {
+          break;
+        }
       }
+      ans = new int[size];
+      ans[k++] = nums[i];
+
+      preorderHB(nums, 0, i - 1);
+      preorderHB(nums, i + 1, size - 1);
+
+      return ans;
     }
-    ans = new int[size];
-    ans[k++] = nums[i];
 
-    preorderHB(nums, 0, i - 1);
-    preorderHB(nums, i + 1, size - 1);
+    void preorderHB(int a[], int l, int h) {
+      if (l > h)
+        return;
+      int mid = (l + h) / 2;
+      ans[k++] = a[mid];
+      preorderHB(a, l, mid - 1);
+      preorderHB(a, mid + 1, h);
 
-    return ans;
+    }
   }
 
-  void preorderHB(int a[], int l, int h) {
-    if (l > h)
-      return;
-    int mid = (l + h) / 2;
-    ans[k++] = a[mid];
-    preorderHB(a, l, mid - 1);
-    preorderHB(a, mid + 1, h);
+  class Solution2 {
+    class Node {
+      int val;
+      Node left;
+      Node right;
 
+      Node(int val) {
+        this.val = val;
+        left = right = null;
+      }
+    }
+
+    public Node sortedArrayToBST(int[] nums) {
+      return createBST(nums, 0, nums.length - 1);
+    }
+
+    Node createBST(int[] nums, int low, int high) {
+
+      if (low > high)
+        return null;
+
+      int mid = (low + high) / 2;
+
+      Node root = new Node(nums[mid]);
+
+      root.left = createBST(nums, low, mid - 1);
+      root.right = createBST(nums, mid + 1, high);
+
+      return root;
+    }
   }
 }
