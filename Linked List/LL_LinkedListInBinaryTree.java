72bf7a8a6291c9a0826fc4ee8e645b9899509e28
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-09-07 09:15:07 +0530

    linked list in binary tree

---
+++ Linked List/LL_LinkedListInBinaryTree.java
@@ -0,0 +1,61 @@
+
+public class LL_LinkedListInBinaryTree {
+
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public boolean isSubPath(ListNode head, TreeNode root) {
+
+    if (root == null)
+      return false;
+
+    return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+
+  }
+
+  private boolean dfs(ListNode head, TreeNode root) {
+
+    if (head == null)
+      return true;
+    if (root == null)
+      return false;
+
+    if (head.val != root.val)
+      return false;
+
+    return dfs(head.next, root.left) || dfs(head.next, root.right);
+  }
+}
