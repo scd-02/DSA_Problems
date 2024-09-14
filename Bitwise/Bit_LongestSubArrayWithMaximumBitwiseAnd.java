76203f4e01517f19ee304ae2908fa8922e8c4461
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-09-14 10:55:33 +0530

    longest subarray with maximum bitwise and

---
+++ Bitwise/Bit_LongestSubArrayWithMaximumBitwiseAnd.java
@@ -0,0 +1,22 @@
+public class Bit_LongestSubArrayWithMaximumBitwiseAnd {
+  public int longestSubarray(int[] nums) {
+
+    int length = 0;
+    int max = 0;
+    for (int i = 0; i < nums.length; i++) {
+      max = Math.max(max, nums[i]);
+    }
+
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == max) {
+        count++;
+        length = Math.max(length, count);
+      } else {
+        count = 0;
+      }
+    }
+    return length;
+
+  }
+}
